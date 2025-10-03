5daac6b06e602d040c4756e1802375c992d84610
cavin7777 <caven7777@gmail.com>
2025-10-03 10:36:02 +0400

    8_0

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(97, 123):
+            print('{}' .format(i), end="")
+        else:
+            print('{}' .format(i), end="")
+    return (str)
