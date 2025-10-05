46d9c4dd1cce2f189063273ad71aae9666652000
cavin7777 <caven7777@gmail.com>
2025-10-05 15:02:04 +0400

    3_3_not_sum

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,8 +6,12 @@ import sys
 def main():
     argv = sys.argv[1:]
     argc = len(argv)
+    total = 0
 
-    print(sum(int(argv[i])))
+    for i in range(argc):
+        total += int(argv[i])
+    
+    print(total)
 
 
 if __name__ == "__main__":
