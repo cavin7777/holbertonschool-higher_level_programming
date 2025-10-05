d6dd3730c5ba3580cc23c43f02e19a44a0fe2463
cavin7777 <caven7777@gmail.com>
2025-10-05 10:39:23 +0400

    6_2_last_digit

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(0, 10):
     for j in range(i, 10):
-        if i != j:
-            print('{}{}, '.format(i, j), end="")
-        elif i == 8 and j == 9:
-            print('{}{}' .format(i, j))
+        if i == 8 and j == 9:
+            print('{}{}'.format(i, j))
+        elif i != j:
+            print('{}{}, ' .format(i, j), end="")
