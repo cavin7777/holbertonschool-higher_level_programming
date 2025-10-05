d23c89396e4ba8a50150df211b71a20c8b10ea9c
cavin7777 <caven7777@gmail.com>
2025-10-05 13:33:06 +0400

    2_0_OK

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+  
+argv = sys.argv[1:]
+argc = len(argv)
+
+if argc == 0:
+    print('0 arguments.')
+else:
+    if argc == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(argc))
+    for i in range(argc):
+        print('{} : {}'.format(i + 1, argv[i]))
