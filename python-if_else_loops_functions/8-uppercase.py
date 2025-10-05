3401518eebc20f8d66a89cfe443ea9aee25b2aab
cavin7777 <caven7777@gmail.com>
2025-10-05 10:49:08 +0400

    8_2_upper_new_line

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,8 @@
 def uppercase(str):
     for i in str:
         if ord(i) in range(97, 123):
-            print('{}' .format(i), end="")
+            print('{}'.format(chr(ord(i) - 32)), end="")
         else:
-            print('{}' .format(i), end="")
+            print('{}'.format(i), end="")
+    print()
     return (str)
