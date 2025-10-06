8b5797bfb7c7a7b9b5937342b80375abe54cb9cd
cavin7777 <caven7777@gmail.com>
2025-10-06 13:07:15 +0400

    2_3_corr_my_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,5 +4,4 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return None
     my_list[idx] = element
-    print(my_list)
-    print(my_list)
+    return my_list
