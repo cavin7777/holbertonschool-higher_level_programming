cb34fe67cbc0a7d871bcdb726bd98ceddba0323b
cavin7777 <caven7777@gmail.com>
2025-10-06 12:39:08 +0400

    2-main_cor

--- python-data_structures/2-main.py
+++ python-data_structures/2-main.py
@@ -2,6 +2,6 @@
 replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
+idx = 15
 new_element = 9
 new_list = replace_in_list(my_list, idx, new_element)
