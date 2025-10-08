c78e6a89d95aae27d9c4e423c01e1de9dbb28450
cavin7777 <caven7777@gmail.com>
2025-10-08 11:52:02 +0400

    7_2_dict_null

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    if a_dictionary == {}:
+        a_dictionary.keys = key
+        a_dictionary.values = value
+
     for i in a_dictionary.keys():
         if i == key:
             a_dictionary[i] = value
