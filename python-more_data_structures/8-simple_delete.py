5aa520d55d25314edf9b900f9fe86f5bf90b9072
cavin7777 <caven7777@gmail.com>
2025-10-08 13:42:54 +0400

    8_2_OK

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    for i in (a_dictionary.keys()):
-        if i == key:
-            a_dictionary.pop(i)
-        else:
-            a_dictionary
-        return a_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
