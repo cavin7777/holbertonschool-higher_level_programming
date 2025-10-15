7c872e12a3b47fbb120eb11894d6cc6e8d2632c6
cavin7777 <caven7777@gmail.com>
2025-10-15 12:40:10 +0400

    2_2_pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,6 +3,8 @@
 """
 Define square empty.
 """
+
+
 class Square:
     """
     Square Class.
@@ -10,7 +12,7 @@ class Square:
 
     def __init__(self, size=0):
         """function size no attribute.
-        
+
         Parameters:
         size:
 
@@ -25,4 +27,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-            
