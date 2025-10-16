ffe64ba27b82276360b85c2a36d480ef1e62aab3
cavin7777 <caven7777@gmail.com>
2025-10-16 12:44:35 +0400

    1_2_private

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     Rectangle Class.
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     # GETTER METHOD
     @property
