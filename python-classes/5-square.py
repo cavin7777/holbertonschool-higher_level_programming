bbc6fa2c73df820cc14d02c57b3304538dce4e83
cavin7777 <caven7777@gmail.com>
2025-10-16 09:33:12 +0400

    5_0

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+Define square empty.
+"""
+
+
+class Square:
+    """
+    Square Class.
+    """
+
+    def __init__(self, size=0):
+        """function size no attribute.
+
+        Parameters:
+        size:
+
+        Raises:
+        TypeError:
+        ValueError:
+
+        """
+        self.__size = size
+
+    # getter method
+    @property
+    def size(self):
+        return self.__size
+
+    # setter method
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
