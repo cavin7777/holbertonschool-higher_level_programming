19831d6884cba1f310aaef300d783824961602c6
cavin7777 <caven7777@gmail.com>
2025-10-22 11:52:00 +0400

    6_0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
