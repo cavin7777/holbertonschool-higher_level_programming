e84925cf0afef3b3112f277f48e1ac659d764c18
cavin7777 <caven7777@gmail.com>
2025-10-24 14:23:58 +0400

    1_10_comments

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,56 +2,57 @@
 from abc import ABC, abstractmethod
 import math
 
-"""
-Shape
-"""
+""" Shape """
 
 
 class Shape(ABC):
-    """
-    Class Shape
-    """
+    """ Class Shape """
+
     @abstractmethod
     def area(self):
+        """ Calculate area """
         pass
 
     @abstractmethod
     def perimeter(self):
+        """ Calculate perimeter """
         pass
 
 
 class Circle(Shape):
-    """
-    Class Circle
-    """
+    """ Class Circle """
+
     def __init__(self, radius):
+        """ Initiate radius"""
         self.radius = radius
 
     def area(self):
+        """ Area circle"""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
+        """ Perimeter Circle """
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """
-    Class Rectangle
-    """
+    """ Class Rectangle """
+
     def __init__(self, width, height):
+        """ Initiate width and height"""
         self.width = width
         self.height = height
 
     def area(self):
+        """ Area rectangle """
         return self.height * self.width
 
     def perimeter(self):
+        """ Perimeter rectangle """
         return 2 * (self.height + self.width)
 
 
 def shape_info(self):
-    """
-    Duck Typing
-    """
+    """ Duck Typing """
     print(f"Area: {self.area()}")
     print(f"Perimeter: {self.perimeter()}")
