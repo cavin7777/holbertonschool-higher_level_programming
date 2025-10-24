7a610b99ac0c93aebf5cc56a8a59cb9d712734e5
cavin7777 <caven7777@gmail.com>
2025-10-24 14:38:30 +0400

    1_13_comments

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,57 +1,57 @@
 #!/usr/bin/python3
-""" Task 2 Duck typing """
+"""Task 2 Duck typing"""
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
-    """ Class Shape """
+    """Class Shape"""
 
     @abstractmethod
     def area(self):
-        """ Calculate area """
+        """Calculate area"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """ Calculate perimeter """
+        """Calculate perimeter"""
         pass
 
 
 class Circle(Shape):
-    """ Class Circle """
+    """Class Circle"""
 
     def __init__(self, radius):
-        """ Initiate radius"""
+        """Initiate radius"""
         self.radius = radius
 
     def area(self):
-        """ Area circle"""
+        """Area circle"""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """ Perimeter Circle """
+        """Perimeter Circle"""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """ Class Rectangle """
+    """Class Rectangle"""
 
     def __init__(self, width, height):
-        """ Initiate width and height"""
+        """Initiate width and height"""
         self.width = width
         self.height = height
 
     def area(self):
-        """ Area rectangle """
+        """Area rectangle"""
         return self.height * self.width
 
     def perimeter(self):
-        """ Perimeter rectangle """
+        """Perimeter rectangle"""
         return 2 * (self.height + self.width)
 
 
 def shape_info(shape):
-    """ Duck Typing """
+    """Duck Typing"""
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
