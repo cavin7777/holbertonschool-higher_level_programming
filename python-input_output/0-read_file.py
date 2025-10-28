380511bf8d34f494e0e15a4c9209c599b1d40c3b
cavin7777 <caven7777@gmail.com>
2025-10-28 11:30:00 +0400

    0_2_documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+""" Read file"""
+
 def read_file(filename=""):
+    """ Read file """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
