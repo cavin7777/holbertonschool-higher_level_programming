11d105cdf982508fa05dffd17c3df297c75611c3
cavin vencadoo <caven7777@gmail.com>
2025-11-20 11:30:24 +0400

    Fix user creation syntax by removing authentication plugin

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- CREATE, ALL PRI, TO NEW USER
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
