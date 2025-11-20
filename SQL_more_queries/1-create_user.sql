18f8ba26371f5e4f60cb7acd98be389e89ca4f6f
cavin7777 <caven7777@gmail.com>
2025-11-20 11:36:57 +0400

    1_4

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- CREATE, ALL PRI, TO NEW USER
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
---ALL PRIVILEGES
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Recomadation : To reload the grant tables to ensure that the new privileges are put into effect
