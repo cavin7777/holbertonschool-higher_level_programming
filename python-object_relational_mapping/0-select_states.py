104df8a635441afbb1b10317ff458c8ba11326a7
cavin7777 <caven7777@gmail.com>
2025-11-25 12:54:20 +0400

    0_2_arguemnts

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,28 +2,37 @@
 """
 0-select_states.py
 Lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
 """
 
-import pymysql
-pymysql.install_as_MySQLdb()  # Makes PyMySQL work as MySQLdb
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Get arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <username> <password> <database>")
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     # Connect to MySQL
     db = MySQLdb.connect(
-        host="localhost",   # your MySQL server
-        user="root",        # your username
-        passwd="cavin",     # your password
-        db="hbtn_0e_0_usa",# your database
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=database,
         port=3306
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")  # Query all states ordered by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
     rows = cursor.fetchall()
 
     for row in rows:
-        print(row)  # Each row is a tuple like (1, 'California')
+        print(row)
 
     cursor.close()
     db.close()
