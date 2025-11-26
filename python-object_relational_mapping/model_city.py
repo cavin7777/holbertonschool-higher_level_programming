96604fde56e26e4c305f842673938dac240372c0
cavin7777 <caven7777@gmail.com>
2025-11-26 11:44:29 +0400

    model_city_pycodestyle

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """City class mapped to cities table"""
     __tablename__ = 'cities'
