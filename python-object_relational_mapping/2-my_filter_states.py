d9bcbce3186f6954b6d2cfee46c0a6c248d1427b
cavin7777 <caven7777@gmail.com>
2025-11-26 09:08:16 +0400

    2_0

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Lists states where name matches user input"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    search_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    # Query using format() (unsafe on purpose for this task)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(search_name)
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
