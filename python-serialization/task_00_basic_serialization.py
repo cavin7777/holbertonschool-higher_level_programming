782acdfd5728fdff4899864dcf03aedea45f9790
cavin7777 <caven7777@gmail.com>
2025-12-24 12:17:50 +0400

    0_1

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,6 +8,7 @@ def serialize_and_save_to_file(data, filename):
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(data, file)
 
+
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
     with open(filename, "r", encoding="utf-8") as file:
